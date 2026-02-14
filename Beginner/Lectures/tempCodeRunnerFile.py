28bc0e1779f22bfc717bf6b74d4e15252db301cf
actionxbar-14 <ankitpaths144@gmail.com>
2026-02-14 09:18:01 +0530

    done

--- Beginner/Lectures/tempCodeRunnerFile.py
+++ Beginner/Lectures/tempCodeRunnerFile.py
@@ -1,4 +1,10 @@
+rt_by_values(data):
+#    sorted_items = sorted(data.items() , key = lambda item: item[1])
 
-# ::  Set Methods : 
+#    return {key:value for key , value in sorted_items}
 
-# 1. Union : Combines elements from two sets , removing duplicates.
+
+
+
+
+# print(sort_by_values(data))   # Output : {'c': 2, 'a': 5, 'd': 7, 'b': 9}
