f7a48989326873d634696650a2a17a26067472ff
actionxbar-14 <ankitpaths144@gmail.com>
2026-02-16 06:05:48 +0530

    OOPs - Part 2 Completed!!

--- Beginner/Lectures/Lecture[24].py
+++ Beginner/Lectures/Lecture[24].py
@@ -124,40 +124,40 @@
 #----> #  __init__ method - constructor , value initialized : fixed.
 #----> #  refrence or connection build between class and object : fixed
 
-class Student :
+# class Student :
 
-#----> # __init__  Method
-    def __init__(self , full_name , Curr_age, percentage,team):  
-        self.name = full_name   #--> attribute (variable)
-        self.age =  Curr_age    #--> attribute (variable)
-        self.percentage =  percentage    #--> attribute (variable)
-        self.team = team
+# #----> # __init__  Method
+#     def __init__(self , full_name , Curr_age, percentage,team):  
+#         self.name = full_name   #--> attribute (variable)
+#         self.age =  Curr_age    #--> attribute (variable)
+#         self.percentage =  percentage    #--> attribute (variable)
+#         self.team = team
 
-#----> # Custom Method  
-    def student_details(self):
-        print(f"{self.name} is {self.age} years old have {self.percentage} % and is in team {self.team}")
-
-
-
-team1 = 'A'
-team2 = 'B'
-#----> # Creating Objects
-student1 = Student("Anubhav" , 21, 99,team1)    
+# #----> # Custom Method  
+#     def student_details(self):
+#         print(f"{self.name} is {self.age} years old have {self.percentage} % and is in team {self.team}")
 
 
-print(student1.name , student1.age)   # Output : Anubhav 21
-student1.student_details()     # Output : Anubhav is 21 years old have 99 % and is in team A
 
-print(student1.__dict__)     # Output :    {'name': 'Anubhav', 'age': 21, 'percentage': 99, 'team': 'A'} 
-
-#---> Modify object property
-student1.percentage = 95
-student1.student_details()   # Output : Anubhav is 21 years old have 95 % 
+# team1 = 'A'
+# team2 = 'B'
+# #----> # Creating Objects
+# student1 = Student("Anubhav" , 21, 99,team1)    
 
 
-#----> delete object property
-# del student1.percentage
-# print(student1.__dict__)    # Output : {'name': 'Anubhav', 'age': 21} 
+# print(student1.name , student1.age)   # Output : Anubhav 21
+# student1.student_details()     # Output : Anubhav is 21 years old have 99 % and is in team A
+
+# print(student1.__dict__)     # Output :    {'name': 'Anubhav', 'age': 21, 'percentage': 99, 'team': 'A'} 
+
+# #---> Modify object property
+# student1.percentage = 95
+# student1.student_details()   # Output : Anubhav is 21 years old have 95 % 
+
+
+# #----> delete object property
+# # del student1.percentage
+# # print(student1.__dict__)    # Output : {'name': 'Anubhav', 'age': 21} 
 
 
 
@@ -177,16 +177,16 @@ student1.student_details()   # Output : Anubhav is 21 years old have 95 %
 
 
 #----> # Creating Objects
-student2 = Student("Neha" , 21, 90,team2)
-print(student2.name , student2.age)   # Output :  Neha 21
-student2.student_details()     # Output :   Neha is 21 years old have 90 %  and is in team B
+# student2 = Student("Neha" , 21, 90,team2)
+# print(student2.name , student2.age)   # Output :  Neha 21
+# student2.student_details()     # Output :   Neha is 21 years old have 90 %  and is in team B
 
-print(student2.__dict__)       # Output : {'name': 'Neha', 'age': 21, 'percentage': 90, 'team': 'B'}
+# print(student2.__dict__)       # Output : {'name': 'Neha', 'age': 21, 'percentage': 90, 'team': 'B'}
 
 
-#---> Modify object property
-student2.percentage = 99
-student2.student_details()   # Output : Neha is 21 years old have 99 % 
+# #---> Modify object property
+# student2.percentage = 99
+# student2.student_details()   # Output : Neha is 21 years old have 99 % 
 
 
 
@@ -205,6 +205,318 @@ student2.student_details()   # Output : Neha is 21 years old have 99 %
 
 # :----> Self parameter :  the self parameter is the refrence to the current instance of the class and is use to access variables that belong to the class. (class and object ke beech mai relation create krta hai)
 
-# __________________________________________________________________________________________________________
+# __________________________________________________________________________________________________________________
+
+
+
+
+# :: 4 - features in OOPs :
+
+# 1. Abstraction
+# 2. Encapsulation
+# 3. Inheritance
+# 4. Polymorphism
+
+
+
+
+
+
+
+
+# 1. Abstraction  : 
+# - Hiding unnecessary details from users through class , methods.
+
+
+
+# Ex:
+
+
+# class Student:
+
+#     def __init__(self , name , age ,  grade , percentage):
+#         self.name = name
+#         self.age = age
+#         self.grade = grade
+#         self.percentage = percentage
+
+
+#     def print_student_details(self):  # methon - abstraction
+#         print(f"The name of student is {self.name} is {self.age} years old , got grade {self.grade} and {self.percentage + 1} %")
+
+
+
+
+# # Object - instance of class
+# student1 = Student("Anubhav" , 21 , 'A' , 95)
+# student1.print_student_details()
+# print(student1.__dict__)
+
+
+
+# student2 = Student("Neha" , 21 , 'A+' , 97)
+# student1.print_student_details()
+# print(student2.__dict__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 2. Encapsulation : Restrict access to certain attributes or methods to protect data and enforce controlled access.
+
+
+# Ex:
+
+
+
+
+# class Student:
+
+#     def __init__(self , name , age ,  grade , percentage):
+#         self.name = name
+#         self.age = age
+#         self.grade = grade
+#         self.__percentage = percentage   #---> Double underscore limit access ( private variable )
+
+
+    
+#     def get_percentage(self):
+
+#         return self.__percentage
+
+
+
+#     def print_student_details(self):  # methon - abstraction
+#         print(f"The name of student is {self.name} is {self.age} years old , got grade {self.grade} and {self.percentage} %")
+
+    
+
+
+
+# Object - instance of class
+# student1 = Student("Anubhav" , 21 , 'A' , 95)
+# student1.print_student_details()
+# print(student1.__dict__)
+
+
+
+# print(student1.percentage)   # Output : AttributeError: 'Student' object has no attribute 'percentage'
+
+
+
+# print(student1.__percentage)  # Output :  AttributeError: 'Student' object has no attribute 'percentage'
+        #   ^^^^^^^^^^^^^^^^^^^     
+# AttributeError: 'Student' object has no attribute 'percentage' 
+
+# print(student1.get_percentage())   # Output : 95
+
+
+
+
+# student2 = Student("Neha" , 21 , 'A+' , 97)
+# student1.print_student_details()
+# print(student2.__dict__)     
+
+
+
+
+# print(student1.percentage)   # Output : AttributeError: 'Student' object has no attribute 'percentage'
+
+
+
+# print(student1.__percentage)  # Output :  AttributeError: 'Student' object has no attribute 'percentage'
+        #   ^^^^^^^^^^^^^^^^^^^     
+# AttributeError: 'Student' object has no attribute 'percentage' 
+
+
+# print(student2.get_percentage())  # Output : 97
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3. Inheritance : 
+# - Allow one class ( child ) to reuse the prop and methods of another class ( parent ).
+
+
+
+
+# Ex:
+
+
+# #-----> this is parent class
+# class Student:
+
+#     def __init__(self , name , age ,  grade , percentage):
+#         self.name = name
+#         self.age = age
+#         self.grade = grade
+#         self.percentage = percentage
+
+
+#     def print_student_details(self):  # methon - abstraction
+#         print(f"The name of student is {self.name} is {self.age} years old , got grade {self.grade} and {self.percentage + 1} %")
+
+
+
+
+# # Object - instance of class
+# student1 = Student("Anubhav" , 21 , 'A' , 95)
+# # student1.print_student_details()
+# # print(student1.__dict__)
+
+
+
+# student2 = Student("Neha" , 21 , 'A+' , 97)
+# # student1.print_student_details()
+# # print(student2.__dict__)
+
+
+
+
+
+# #-----> this is child class
+
+# # Graduatestudent child class inherit prop and methods from Student Parent class
+# class GraduateStudent(Student):
+
+#     def __init__(self, name, age, grade, percentage, stream): # here we can take old parameters from parent class and new parameters in child class.
+#         self.stream = stream  # new attribute in child class
+#         super().__init__(name ,age, grade , percentage,)  #---> Super() calls the parent class init
+        
+   
+#     def student_details(self):
+#         super().print_student_details() # method from parent class -- > print(f"stream is {self.stream}")
+        
+     
+
+# Grad_Student1 = GraduateStudent('keshav', 'A',22,  96, 'PCM')
+# # print(Grad_Student1.name)   # Output : keshav
+# # print(Grad_Student1.stream) # Output : PCM
+# Grad_Student1.student_details() # Output : The name of student is keshav is A years old , got grade 22 and 97 %
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 4. Polymorphism : 
+
+# - Allow a method of one function use in another class with different or multiple functionality.
+
+
+
+# Ex:
+
+
+
+
+
+#-----> this is parent class
+class Student:
+
+    def __init__(self , name , age ,  grade , percentage):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        self.percentage = percentage
+
+
+    def print_student_detail(self):  # methon - abstraction
+        print(f"The name of student is {self.name} is {self.age} years old , got grade {self.grade} and {self.percentage + 1} %")
+
+
+
+
+# Object - instance of class
+student1 = Student("Anubhav" , 21 , 'A' , 95)
+# student1.print_student_details()
+# print(student1.__dict__)
+
+
+
+student2 = Student("Neha" , 21 , 'A+' , 97)
+# student1.print_student_details()
+# print(student2.__dict__)
+
+
+
+
+
+#-----> this is child class
+
+# Graduatestudent child class inherit prop and methods from Student Parent class
+class GraduateStudent(Student):
+
+    def __init__(self, name, age, grade, percentage, stream): 
+        self.stream = stream  # new attribute in child class
+        super().__init__(name ,age, grade , percentage,)  #---> Super() calls the parent class init
+        
+   
+    def print_student_detail(self):   
+        # super().print_student_details() # method from parent class -- > print(f"stream is {self.stream}")
+        print("this is the method of child class")
+        
+     
+
+Grad_Student1 = GraduateStudent('keshav', 'A',22,  96, 'PCM')
+# print(Grad_Student1.name)   # Output : keshav
+# print(Grad_Student1.stream) # Output : PCM
+student1.print_student_detail()
+Grad_Student1.print_student_detail() 
+
+
 
 
