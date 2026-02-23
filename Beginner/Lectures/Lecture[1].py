3178538ec2dbaa90438ebaaf51240e615d4d656f
actionxbar-14 <ankitpaths144@gmail.com>
2026-02-23 23:04:01 +0530

    revision!!

--- Beginner/Lectures/Lecture[1].py
+++ Beginner/Lectures/Lecture[1].py
@@ -7,7 +7,7 @@
 
 
 a = "Anubhav"
-print(a);
+print(a)
 
 
 
@@ -17,17 +17,21 @@ print(a);
 #          greeting = "Hello World"
 #          print(greeting)  # Output: Hello World
 
+
 a = 1     # integer number
 print(a)
 # Output: 1
+
 
 b = 1.1357101   #decimal number
 print(b)
 # Output: 1.1357101
 
+
 c = "Anubhav"   # word / sentence - use quotes
 print(c)
 # Output: Anubhav
+
 
 
 d = anubhav
@@ -36,17 +40,28 @@ print(d)
 #         ^^^^^^^
 # NameError: name 'anubhav' is not defined
 
+
 anubhav = 14   #declare a variable using variable
 d = anubhav
 print(d)
+# Output : 14
+
 
 x = 10  #adding a number to a variable
 print(x+1)
 # output : 11
 
+
 p,q,r = 11,12,13  #assign multiple variables
 print(p,q,r)
 # output : 11 12 13 
+
+
+
+
+
+
+
 
 
 # :: Ways to declare a variable name :
@@ -56,8 +71,20 @@ myname = "Anubhav"  # flat case.
 my_name = "Anubhav" # snake case.
 
 
+
+
+
+
 # :: NOTE : 
-# Python primarily uses snake_case for variables, functions, methods, and module names, as recommended by PEP 8. For class names, Python uses PascalCase (or CapWords), and for constants, it uses screaming_snake_case (all uppercase with underscores). This convention improves readability and distinguishes between different types of identifiers.
+# - Python primarily uses snake_case for variables, functions, methods, and module names, as recommended by PEP 8.
+#-  For class names, Python uses PascalCase (or CapWords), and for constants, it uses screaming_snake_case (all uppercase with underscores).
+# - This convention improves readability and distinguishes between different types of identifiers.
+
+
+
+
+
+
 
 
 # :: Basic rule to declare variable :-
--- Beginner/Lectures/Practice.py
+++ Beginner/Lectures/Practice.py
@@ -155,13 +155,16 @@
 
 
 
-def Fibonacci(n):
-    a,b = 0,1
-    count = 0
+# def Fibonacci(n):
+#     a,b = 0,1
+#     count = 0
 
-    while count < n:
-        print(a)
-        a,b = b , a+b 
-        count += 1
+#     while count < n:
+#         print(a)
+#         a,b = b , a+b 
+#         count += 1
 
-Fibonacci(10)
+# Fibonacci(10)
+
+
+
