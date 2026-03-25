d6e2fc1cc81fa0f716bf7a99b2d0aa9839fa3d66
actionxbar-14 <ankitpaths144@gmail.com>
2026-03-25 19:46:19 +0530

    folder name changed

---
+++ Intermidiate/Lectures/Lecture[6].py
@@ -0,0 +1,10 @@
+
+# :: Youtube Playlist Content : 
+
+# --> lecture-31 : Indentation in Python.
+# --> lecture-32 : If statement in Python. 
+# --> lecture-33 : If-else statement in Python. 
+# --> lecture-34 : nested If-else in Python / Short hand if's. 
+# --> lecture-35 : if-elif-else in Python / multiple conditions.
+
+
---
+++ Intermidiate/Lectures/Lecture[7].py
@@ -0,0 +1,11 @@
+
+# :: Youtube Playlist Content : 
+
+# --> lecture-36 : While loop in Python.
+# --> lecture-37 : While else in Python / nested while loop in Python. 
+# --> lecture-38 : range() function. 
+# --> lecture-39 : For loop / for else in Python. 
+# --> lecture-40 : Break and Continue in Python.
+
+
+
