7f81efd0631f16b6cd4997a4208bf989126c677c
actionxbar-14 <ankitpaths144@gmail.com>
2026-06-13 17:00:02 +0530

    Lecture 46 Started!!

--- Intermidiate/Lectures/Lecture[46].py
+++ Intermidiate/Lectures/Lecture[46].py
@@ -8,4 +8,65 @@
 # --> lecture-235 : Revision. 
 
 
-# _______________________________________________________________________________________________________________________ 
+# ______________________________________________________________________________________________________________________________________
+
+
+
+# :: What is ADT ( Abstract Data Type ) : 
+# -  ADT's are like user defined datatypes which define operations on values using function without specifying what is there inside a function and how operations are performed. 
+# - You can think ADT as a black box which hides inner structure and design of datatypes from user. 
+
+
+
+
+# - Ex: 
+
+# list_1 = [10 , 20 , 'kamli' , 20.4]
+
+# --> Find the length of above list : len(list)
+
+# Explanation :  You don't know the actual or internal implementation of len() function. 
+
+
+
+
+
+# STACK ADT : 
+
+
+# :--> Operations :- 
+
+# 1.) Initialise :- create an instance of stack. 
+# 2.) Push :- adds an element at the end. 
+# 3.) Pop :- removes element from stack. 
+# 4.) IsEmpty :- Checks stack is empty or not.
+# 5.) IsFull :- Checks stack is full or not.
+
+
+
+
+
+
+
+# :: Advantages :- 
+
+# 1. Implementation of stack : List , array  , linked list , numpy array. 
+
+# 2. Reduce development time because of pre-defined implementations. 
+
+# 3. Less bugs and Helps in debugging. 
+
+# 4. Easy to change implementations. 
+
+
+
+
+
+# ______________________________________________________________________________________________________________________________________
+
+
+
+
+
+# :: Top 21 Magic Methods and Attributes : 
+
